b8153b946b5019e5370872057d6b3576d3dba3ed
mahtab.mahdavi <mahtaabmahdavi@gmail.com>
2023-08-21 09:16:33 +0330

    initialize configuration of the project

---
+++ Mahtab/src/main/java/com/Hammasir/Mahtab/HammasirSpringBootFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.Hammasir.Mahtab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammasirSpringBootFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HammasirSpringBootFrameworkApplication.class, args);
+	}
+
+}
---
+++ Mahtab/src/test/java/com/Hammasir/Mahtab/HammasirSpringBootFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hammasir.Mahtab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HammasirSpringBootFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
