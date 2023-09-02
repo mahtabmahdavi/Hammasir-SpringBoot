106632e2673995492b56e050144050a8c5545362
mahtab.mahdavi <mahtaabmahdavi@gmail.com>
2023-09-02 10:15:32 +0330

    command line runner task is done

--- src/main/java/com/Hammasir/Mahtab/RestaurantSpringApplication.java
+++ src/main/java/com/Hammasir/Mahtab/RestaurantSpringApplication.java
@@ -1,28 +1,14 @@
 package com.Hammasir.Mahtab;
 
-import com.Hammasir.Mahtab.config.ConfigService;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import jakarta.annotation.PostConstruct;
-
 @SpringBootApplication
 @EnableScheduling
 public class RestaurantSpringApplication {
-	private final ConfigService configService;
-
-	public RestaurantSpringApplication(ConfigService configService) {
-		this.configService = configService;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run( RestaurantSpringApplication.class, args);
 	}
 
-	@PostConstruct
-	public void init() {
-		configService.printApiUrl();
-	}
 }
---
+++ src/main/java/com/Hammasir/Mahtab/config/ConfigCommandLineRunner.java
@@ -0,0 +1,16 @@
+package com.Hammasir.Mahtab.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+
+@Component
+public class ConfigCommandLineRunner implements CommandLineRunner {
+    @Value("${config.name}")
+    private String configName;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Value from config file (Command Line Runner): " + configName);
+    }
+}
