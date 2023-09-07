46119e9c81936498bacd8b3811ce75d0413eec6e
mahtab.mahdavi <mahtaabmahdavi@gmail.com>
2023-09-07 21:36:51 +0330

    initialize repositories

---
+++ springboot/src/main/java/com/Hammasir/springboot/repository/MenuRepository.java
@@ -0,0 +1,11 @@
+package com.Hammasir.springboot.repository;
+
+import com.Hammasir.springboot.model.Menu;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+}
---
+++ springboot/src/main/java/com/Hammasir/springboot/repository/RestaurantRepository.java
@@ -0,0 +1,11 @@
+package com.Hammasir.springboot.repository;
+
+import com.Hammasir.springboot.model.Restaurant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+}
